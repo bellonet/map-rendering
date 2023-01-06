48a8de27ef9516768ab236fea22d05b96e7700c1
bellonet <bellonet@gmail.com>
2023-01-06 22:16:45 +0100

    solution calls a seperate python script

---
+++ album_solution/render_movie_heatflux_timepoints.py
@@ -0,0 +1,121 @@
+import pyvista as pv
+import numpy as np
+import netCDF4 as nc
+import gemgis as gg
+
+def create_timepoint_mesh(ds, heat_arr_name, fill_value, tp_idx, n_smooth_itr):
+
+    arr = ds[heat_arr_name][tp_idx]
+    min_val = np.min(arr)
+
+    ## due to sometimes producing an empty / incorrect mesh:
+    ## try_generate_mesh func
+    n_points = 0
+    z_min_bound = fill_value
+    while not n_points or z_min_bound==fill_value:
+
+        ## creates a pyvista.StructuredGrid
+        grid = gg.visualization.create_dem_3d(dem=arr.filled(fill_value=fill_value), 
+                                              extent=[0,arr.shape[1],0,arr.shape[0]])
+        
+        grid.rename_array('Elevation','Heat')
+
+        ## get rid of fill_val values:
+        unstructured_grid = grid.threshold(int(min_val-2), invert=False, preference='point', all_scalars=True)
+        n_points = unstructured_grid.n_points
+        z_min_bound = unstructured_grid.bounds[4]
+
+    grid = unstructured_grid
+
+    ## check surface memory /time - maybe needs to be surface and not mesh
+    ## smooth the surface:
+    if n_smooth_itr:        
+        # creates a pyvista.PolyData (surface data)
+        grid = grid.extract_geometry()
+        # Smooth the surface
+        grid = grid.smooth(n_iter=n_smooth_itr)
+
+    return grid
+
+
+def format_date(date):
+    date = str(date)
+    return f'{date[6:8]}/{date[4:6]}/{date[:4]}'
+
+def run_render(dataset_path, output_path, n_timepoints, n_smooth_itr, event_csv, opacity, framerate):
+
+    # dataset:
+    ## preprocessing steps:
+    heat_arr_name = 'surface_upward_sensible_heat_flux' # check with dataset has 3D and get name??
+    ds = nc.Dataset(dataset_path)
+    time_arr = ds['time'][:]
+    long_arr = ds['longitude'][:]
+    lat_arr = ds['latitude'][:]
+    fill_value=-999
+
+    # plotting params:
+    cmap = 'seismic'
+    clim = [-200, 200] # check values outside of range
+    sargs = {"color":"black"} # scale bar arguments
+
+    ## Create our render engine
+    p = pv.Plotter(notebook=False, off_screen=True, 
+                   window_size=[1920*2, 1080*2], multi_samples=16) # multi_sample mitigates aliasing - higher is better, but might affect performance.
+
+    ## Open up an MP4 with a 60 FPS framerate
+    p.open_movie(output_path, framerate=framerate)
+
+    p.set_background('white')
+
+    print(f'rendering time point: 0')
+
+    actor_mesh = p.add_mesh(mesh=create_timepoint_mesh(ds, heat_arr_name, fill_value, 0, n_smooth_itr), clim=clim, cmap=cmap, opacity=opacity, scalar_bar_args=sargs)
+    actor_date = p.add_text(format_date(time_arr[0]), position='upper_right', color='blue', shadow=True, font_size=26)
+
+    # Camera position is a tuple: camera location, focus point, viewup vector
+    # [(x,y,z), (fx,fy,fz,), (nx,ny,nz)]
+
+    p.camera_position = 'xy'
+    p.camera.zoom(1.5)
+
+    ### for event annotations on the dataset:
+    ## create a csv file with this data.
+    ## For now just shows a test example:
+    points = np.array([[200., 200., 150.],])
+    labels = [f'lorem ipsum \n lorem ipsum']
+
+    actor_eventText = p.add_point_labels(points, labels, italic=True, font_size=20,
+                                shape_color='black', name='event_text',
+                                point_color='green', point_size=20,
+                                render_points_as_spheres=True,
+                                always_visible=True, shadow=True)
+
+    p.show(auto_close=False)
+
+    ## Run through each frame
+    p.write_frame()  ## write initial data
+
+    for tp_idx in range(1,n_timepoints):
+
+        print(f'rendering time point: {tp_idx}')
+        p.remove_actor(actor_mesh)
+        p.remove_actor(actor_date)
+        p.remove_actor('event_text')
+
+        actor_mesh = p.add_mesh(mesh=create_timepoint_mesh(ds, heat_arr_name, fill_value, tp_idx, n_smooth_itr), clim=clim, cmap=cmap, opacity=opacity, scalar_bar_args=sargs)
+        actor_date = p.add_text(format_date(time_arr[tp_idx]), position='upper_right', color='blue', shadow=True, font_size=26)
+        
+        if tp_idx==5:
+            actor_eventText = p.add_point_labels(points, labels, italic=True, font_size=20,
+                                shape_color='black', name='event_text',
+                                point_color='green', point_size=20,
+                                render_points_as_spheres=True,
+                                always_visible=True, shadow=True)
+
+        p.write_frame()
+        
+    pv.close_all()
+
+run_render('/home/ella/work/heat_flux_anomalies/UFZ_RemoteSensing/HOLAPS-H-JJA_anomaly-d-2001-2005.nc',
+    'test.mp4',
+    4, 10, None, 0.5, 5)
--- album_solution/solution.py
+++ album_solution/solution.py
@@ -1,8 +1,5 @@
 from album.runner.api import setup
 
-# Please import additional modules at the beginning of your method declarations.
-# More information: https://docs.album.solutions/en/latest/solution-development/
-
 env_file = """channels:
   - conda-forge
   - defaults
@@ -18,134 +15,30 @@ dependencies:
   - imageio-ffmpeg=0.4.7
 """
 
-
-
 def run():
-    from album.runner.api import get_args
-    args = get_args()
-    # print("Hi", str(args.name), "nice to meet you!")
 
-    #import time
-    import pyvista as pv
-    import numpy as np
-    import netCDF4 as nc
-    import gemgis as gg
+    from album.runner.api import get_args
+    import render_movie_heatflux_timepoints
+
+    args = get_args()
+    ## call python.script
 
     ### args:
     dataset_path = str(args.dataset_path)
     ####'/home/ella/work/heat_flux_anomalies/UFZ_RemoteSensing/HOLAPS-H-JJA_anomaly-d-2001-2005.nc'
     output_path = str(args.output_path)
-    n_timepoints = int(args.n_timepoints)
+    n_timepoints = int(args.n_timepoints) ## change to range??
     n_smooth_itr = int(args.n_smooth_itr)
+    event_csv = str(args.event_csv_path)
 
     ## plot args:
     opacity=float(args.opacity)
     framerate=int(args.framerate)
-    cmap = 'seismic'
-    clim = [-200, 200]
 
+    # Print the arguments:
     print(f'\nargs: \ndataset_path:{dataset_path} \noutput_path:{output_path} \nn_timepoints:{n_timepoints} \nsmooth_itr:{n_smooth_itr} \nopacity:{opacity} \nframerate:{framerate}')
 
-    heat_var = 'surface_upward_sensible_heat_flux'
-    fill_value=-999
-    ds = nc.Dataset(dataset_path)
-    arr_shape = ds.variables[heat_var].shape
-    time_arr = ds['time'][:]
-
-    def create_timepoint_mesh(ii):
-
-        arr = ds[heat_var][ii]
-        min_val = np.min(arr)
-
-        ## due to sometimes producing an empty / incorrect mesh:
-        n_points = 0
-        z_min_bound = fill_value
-        while not n_points or z_min_bound==fill_value:
-
-            ## creates a pyvista.StructuredGrid
-            grid = gg.visualization.create_dem_3d(dem=arr.filled(fill_value=fill_value), 
-                                                  extent=[0,arr_shape[2],0,arr_shape[1]])
-            
-            grid.rename_array('Elevation','Heat')
-
-            ## get rid of fill_val values:
-            unstructured_grid = grid.threshold(int(min_val-2), invert=False, preference='point', all_scalars=True)
-            n_points = unstructured_grid.n_points
-            z_min_bound = unstructured_grid.bounds[4]
-
-        grid = unstructured_grid
-
-        ## smooth the surface:
-        if n_smooth_itr:        
-            # creates a pyvista.PolyData (surface data)
-            grid = grid.extract_geometry()
-            # Smooth the surface
-            grid = grid.smooth(n_iter=n_smooth_itr)
-
-        return grid
-
-    sargs = {"color":"black"}
-
-    # initialize the point cloud
-    #update_pc(0)
-
-    ## Create our render engine
-    p = pv.Plotter(notebook=False, off_screen=True, 
-                   window_size=[1920*2, 1080*2], multi_samples=16) # multi_sample mitigates aliasing - higher is better, but might affect performance.
-
-    ## Open up an MP4 with a 60 FPS framerate
-    p.open_movie(output_path, framerate=framerate)
-
-    p.set_background('white')
-
-    print(f'rendering time point: 0')
-
-    actor_mesh = p.add_mesh(mesh=create_timepoint_mesh(0), clim=clim, cmap=cmap, opacity=opacity, scalar_bar_args=sargs)
-    actor_timestamp = p.add_text(f'time stamp: {int(time_arr[0])}', position='upper_right', color='blue', shadow=True, font_size=26)
-
-    # Camera position is a tuple: camera location, focus point, viewup vector
-    # [(x,y,z), (fx,fy,fz,), (nx,ny,nz)]
-
-    p.camera_position = 'xy'
-    p.camera.zoom(1.5)
-
-    ### for event annotations on the dataset:
-    ## For now just shows a test example:
-    points = np.array([[200., 200., 150.],])
-    labels = [f'lorem ipsum \n lorem ipsum']
-
-    actor_eventText = p.add_point_labels(points, labels, italic=True, font_size=20,
-                                shape_color='black', name='event_text',
-                                point_color='green', point_size=20,
-                                render_points_as_spheres=True,
-                                always_visible=True, shadow=True)
-
-    p.show(auto_close=False)
-
-    ## Run through each frame
-    p.write_frame()  ## write initial data
-
-    for i in range(1,n_timepoints):
-
-        print(f'rendering time point: {i}')
-        p.remove_actor(actor_mesh)
-        p.remove_actor(actor_timestamp)
-        p.remove_actor('event_text')
-
-        actor_mesh = p.add_mesh(mesh=create_timepoint_mesh(i), clim=clim, cmap=cmap, opacity=opacity, scalar_bar_args=sargs)
-        actor_timestamp = p.add_text(f'time stamp: {int(time_arr[i])}', position='upper_right', color='blue', shadow=True, font_size=26)
-        
-        if i==5:
-            actor_eventText = p.add_point_labels(points, labels, italic=True, font_size=20,
-                                shape_color='black', name='event_text',
-                                point_color='green', point_size=20,
-                                render_points_as_spheres=True,
-                                always_visible=True, shadow=True)
-
-        p.write_frame()
-        
-    pv.close_all()
-
+    render_movie_heatflux_timepoints.run_render(dataset_path, output_path, n_timepoints, n_smooth_itr, event_csv, opacity, framerate)
 
 setup(
     group="album",
@@ -198,6 +91,12 @@ setup(
         "type": "integer",
         "default": 5,
         "description": "video framerate (frames per second)."
+        },
+        {
+        "name": "event_csv_path",
+        "type": "string",
+        "default": None,
+        "description": "csv with events to display on the map - columns: first date (e.g. 20001231), last date, latitude, longitude"
         },],
     run=run,
     dependencies={'environment_file': env_file}
